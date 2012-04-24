82831c0e2811f8f9bf550a3de865f2fcc5516421
Richard Zhao <richard.zhao@freescale.com>
2012-04-24 14:19:13 +0800

    ARM: imx6q_sabrelite: clk_register_clkdev cko1 for sgtl5000
    Signed-off-by: Richard Zhao <richard.zhao@freescale.com>
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- arch/arm/mach-imx/clk-imx6q.c
+++ arch/arm/mach-imx/clk-imx6q.c
@@ -421,6 +421,9 @@ int __init mx6q_clocks_init(void)
 	clk_register_clkdev(clk[dummy], NULL, "20c0000.wdog");
 	clk_register_clkdev(clk[ssi1_ipg], NULL, "2028000.ssi");
 	clk_register_clkdev(clk[arm], NULL, "cpu");
+	clk_register_clkdev(clk[cko1_sel], "cko1_sel", NULL);
+	clk_register_clkdev(clk[ahb], "ahb", NULL);
+	clk_register_clkdev(clk[cko1], "cko1", NULL);
 
 	for (i = 0; i < ARRAY_SIZE(clks_init_on); i++) {
 		c = clk_get_sys(clks_init_on[i], NULL);
